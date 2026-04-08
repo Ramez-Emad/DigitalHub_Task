41438716ccf53233543e9312dff8beddc107be85
Ramez <ramezemad44@gmail.com>
2026-04-09 01:37:46 +0200

    Refactor Event Driven

---
+++ DigitalHub_Task.Application/CQRS/Commands/Enrollment/EnrollmentCreatedEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace DigitalHub_Task.Application.CQRS.Commands.Enrollment;
+
+
+public record EnrollmentCreatedEvent(
+    int UserId,
+    int CourseId,
+    int EnrollmentId,
+    string EntityName
+) : INotification;
